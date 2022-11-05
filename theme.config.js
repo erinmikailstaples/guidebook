93b0a85b32c2914c20aede6e3eab80a3c02c833c
Erin Mikail Staples <54031531+erinmikailstaples@users.noreply.github.com>
2022-11-05 12:06:02 -0400

    Updated the homepage, added links and icons.

--- theme.config.js
+++ theme.config.js
@@ -1,12 +1,12 @@
 export default {
-  github: 'https://github.com/shuding/nextra',
+  github: 'https://github.com/erinmikailstaples/guidebook',
   docsRepositoryBase: 'https://github.com/shuding/nextra/blob/master',
-  titleSuffix: ' – Nextra',
+  titleSuffix: 'OpenGuide',
   logo: (
     <>
-      <span className="mr-2 font-extrabold hidden md:inline">Nextra</span>
+      <span className="mr-2 font-extrabold hidden md:inline">OpenGuide</span>
       <span className="text-gray-600 font-normal hidden md:inline">
-        The Next.js Static Site Generator
+        A collection of educational guides built in public.
       </span>
     </>
   ),
@@ -16,15 +16,15 @@ export default {
       <meta name="theme-color" content="#ffffff" />
       <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       <meta httpEquiv="Content-Language" content="en" />
-      <meta name="description" content="Nextra: the Next.js site builder" />
-      <meta name="og:description" content="Nextra: the Next.js site builder" />
+      <meta name="description" content="OpenGuide: a collection of educational guides." />
+      <meta name="og:description" content="OpenGuide" />
       <meta name="twitter:card" content="summary_large_image" />
       <meta name="twitter:image" content="https://nextra.vercel.app/og.png" />
-      <meta name="twitter:site:domain" content="nextra.vercel.app" />
-      <meta name="twitter:url" content="https://nextra.vercel.app" />
-      <meta name="og:title" content="Nextra: Next.js static site generator" />
-      <meta name="og:image" content="https://nextra.vercel.app/og.png" />
-      <meta name="apple-mobile-web-app-title" content="Nextra" />
+      <meta name="twitter:site:domain" content="OpenGuide" />
+      <meta name="twitter:url" content="https://twitter.com/guideopen" />
+      <meta name="og:title" content="OpenGuide: a collection of educational guides." />
+      <meta name="og:image" content="https://twitter.com/GuideOpen/photo" />
+      <meta name="apple-mobile-web-app-title" content="OpenGuide" />
       <link
         rel="apple-touch-icon"
         sizes="180x180"
@@ -61,7 +61,7 @@ export default {
   prevLinks: true,
   nextLinks: true,
   footer: true,
-  footerEditLink: 'Edit this page on GitHub',
-  footerText: <>MIT {new Date().getFullYear()} © Nextra.</>,
-  unstable_faviconGlyph: '👋',
+  footerEditLink: 'Contribute to OpenGuide',
+  footerText: <>MIT {new Date().getFullYear()} ©OpenGuide | ©Erin Mikail STaples.</>,
+  unstable_faviconGlyph: '🌐',
 }
